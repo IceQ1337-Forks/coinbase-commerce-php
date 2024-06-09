ccd5e9f21296b5ca889715db8aeb10cc509b8b3a
IceQ1337 <IceQ1337@users.noreply.github.com>
2024-06-09 23:41:14 +0200

    Remove backwards compatibility for `hashEqual`

--- src/Util.php
+++ src/Util.php
@@ -97,21 +97,6 @@ class Util
      */
     public static function hashEqual($str1, $str2)
     {
-        if (\function_exists('hash_equals')) {
-            return \hash_equals($str1, $str2);
-        }
-
-        if (\strlen($str1) != \strlen($str2)) {
-            return false;
-        } else {
-            $res = $str1 ^ $str2;
-            $ret = 0;
-
-            for ($i = \strlen($res) - 1; $i >= 0; $i--) {
-                $ret |= \ord($res[$i]);
-            }
-
-            return !$ret;
-        }
+        return \hash_equals($str1, $str2);
     }
 }
